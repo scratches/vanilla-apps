4f852d58a4017c5c5df8edbf9ea1a670e598dc75
Dave Syer <dsyer@pivotal.io>
2019-07-09 12:03:03 +0100

    Simple component scan with one @Component

---
+++ vanilla-scan/src/main/java/app/main/SampleApplication.java
@@ -0,0 +1,37 @@
+package app.main;
+
+import app.main.model.Foo;
+import app.main.model.FooRepository;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class SampleApplication {
+
+	private FooRepository entities;
+
+	public SampleApplication(FooRepository entities) {
+		this.entities = entities;
+	}
+
+	@Bean
+	public RouterFunction<?> userEndpoints() {
+		return route(GET("/"),
+				request -> ok().body(Mono.fromCallable(() -> entities.findById(1L).get())
+						.subscribeOn(Schedulers.elastic()), Foo.class));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
